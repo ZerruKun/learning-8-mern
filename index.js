34f4188df5f38ff6f7a748237bc3850894d09687
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-08-08 17:48:47 +0300

    Mongoose, БД, создание схемы пользователя.

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/mern", {useNewUrlParser: true})
+    .then(() => console.log("DB ok"))
+    .catch((err) => console.log("DB error", err))
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-})
+app.post("/auth/register", (req, res) => {
 
-app.post("/auth/login", (req, res) => {
-    console.log(req.body);
-
-    const token = jwt.sign({
-        email: req.body.email,
-        fullName: "Ncik Tab"
-    }, "secret123");
-
-    res.json({
-        success: true,
-        token,
-    })
 });
+
+// app.get("/", (req, res) => {
+//     res.send("Hello World!");
+// })
+
+// app.post("/auth/login", (req, res) => {
+//     console.log(req.body);
+
+//     const token = jwt.sign({
+//         email: req.body.email,
+//         fullName: "Ncik Tab"
+//     }, "secret123");
+
+//     res.json({
+//         success: true,
+//         token,
+//     })
+// });
 
 app.listen(4444, (err)=> {
     if(err) {

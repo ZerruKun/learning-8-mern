34d47d2e35f032a71f69c7f368af464fe3fe2311
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-08-08 18:36:28 +0300

    Bcrypt, шифромание пароля.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import { validationResult } from "express-validator";
 
 import {registerValidation} from "./validations/auth.js"
+import UserModel from "./models/User.js"
 
 mongoose.connect("mongodb://127.0.0.1:27017/mern", {useNewUrlParser: true})
     .then(() => console.log("DB ok"))
@@ -13,15 +15,26 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/auth/register", registerValidation, (req, res) => {
+app.post("/auth/register", registerValidation, async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json(errors.array());
     }
 
-    res.json({
-        success: true,
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        avatarUrl: req.body.avatarUrl,
+        passwordHash,
     })
+
+    const user = await doc.save();
+
+    res.json(user);
 });
 
 // app.get("/", (req, res) => {

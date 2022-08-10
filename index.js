87c24bffe239a70f25f31d806fe6afa585d093d9
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-08-10 16:25:21 +0300

    Multer. Загрузка и просмотр изображений.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import multer from "multer";
 import mongoose from "mongoose";
 
 import {registerValidation, loginValidation, postCreateValidation} from "./validations.js";
@@ -15,17 +15,35 @@ mongoose.connect("mongodb://127.0.0.1:27017/mern", {useNewUrlParser: true})
 
 const app = express();
 
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, "uploads");
+    },
+    filename:  (_, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({storage});
+
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 
 app.post("/auth/login", loginValidation, UserController.login);
 app.post("/auth/register", registerValidation, UserController.register);
 app.get("/auth/me", checkAuth, UserController.login);
 
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`,
+    });
+});
+
 app.get("/posts", PostController.getAll);
 app.get("/posts/:id", PostController.getOne);
 app.post("/posts", checkAuth, postCreateValidation, PostController.create);
 app.delete("/posts/:id", checkAuth, PostController.remove);
-app.patch("/posts/:id", PostController.update);
+app.patch("/posts/:id", checkAuth, postCreateValidation, PostController.update);
 
 app.listen(4444, (err)=> {
     if(err) {
